6f5f01784246aba49507e1147b2acc7e3cf710b4
swethak07 <33912977+swethak07@users.noreply.github.com>
2026-07-19 16:06:35 -0500

    Format aws_subnet resource in subnet.tf

--- terraform/subtnet.tf
+++ terraform/subtnet.tf
@@ -25,18 +25,18 @@ resource "aws_subnet" "private_zone2" {
 }
 
 resource "aws_subnet" "public_zone1" {
-    vpc_id=aws_vpc.main.id
-    cidr_block="10.0.64.0/19"
-    availability_zone=local.zone1
-    map_public_ip_on_launch =true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = local.zone1
+  map_public_ip_on_launch = true
 
-    tags = { 
-        "Name" = "${local.env}-public-${local.zone1}"
-        "kubernetes.io/role/elb" = "1"
-        "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-
-    }
+  tags = {
+    "Name"                                                 = "${local.env}-public-${local.zone1}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
 }
+
 
 resource "aws_subnet" "public_zone2" {
     vpc_id=aws_vpc.main.id

4f8e412b47f4c9e63cd795e1a7200e558be7d16d
swetha kliari <skilari86@gmail.com>
2026-07-19 13:55:14 -0500

    push terraform scripts

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = locals.region
+  
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+        source = "harshicorp/aws"
+        version = "~5.49"
+    }    
+
+  }
+}
---
+++ terraform/subtnet.tf
@@ -0,0 +1,54 @@
+resource "aws_subnet" "private_zone1" {
+    vpc_id=aws_vpc.main.id
+    cidr_block="10.0.0.0/19"
+    availability_zone=local.zone1
+
+    tags = { 
+        "Name" = "${locals.env}-private-${locals.zone1}"
+        "kubernetes.io/role/internal-elb" = "1"
+        "kubernetes.io/cluster/${locals.env}-${locals.eks_name}" = "owned"
+
+    }
+}
+
+resource "aws_subnet" "private_zone2" {
+    vpc_id=aws_vpc.main.id
+    cidr_block="10.0.32.0/19"
+    availability_zone=local.zone2
+
+    tags = { 
+        "Name" = "${locals.env}-private-${locals.zone2}"
+        "kubernetes.io/role/internal-elb" = "1"
+        "kubernetes.io/cluster/${locals.env}-${locals.eks_name}" = "owned"
+
+    }
+}
+
+resource "aws_subnet" "public_zone1" {
+    vpc_id=aws_vpc.main.id
+    cidr_block="10.0.64.0/19"
+    availability_zone=local.zone1
+    map_public_ip_on_launch =true
+
+    tags = { 
+        "Name" = "${locals.env}-public-${locals.zone1}"
+        "kubernetes.io/role/elb" = "1"
+        "kubernetes.io/cluster/${locals.env}-${locals.eks_name}" = "owned"
+
+    }
+}
+
+resource "aws_subnet" "public_zone2" {
+    vpc_id=aws_vpc.main.id
+    cidr_block="10.0.96.0/19"
+    availability_zone=local.zone2
+    map_public_ip_on_launch =true
+
+    tags = { 
+        "Name" = "${locals.env}-public-${locals.zone2}"
+        "kubernetes.io/role/elb" = "1"
+        "kubernetes.io/cluster/${locals.env}-${locals.eks_name}" = "owned"
+
+    }
+}
+

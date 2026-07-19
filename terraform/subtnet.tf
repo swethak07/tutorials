bd94d5681aa76c4d7e490719c3eba274d7d06347
swethak07 <33912977+swethak07@users.noreply.github.com>
2026-07-19 15:57:41 -0500

    Change 'local' to 'locals' for availability zones

--- terraform/subtnet.tf
+++ terraform/subtnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "private_zone1" {
     vpc_id=aws_vpc.main.id
     cidr_block="10.0.0.0/19"
-    availability_zone=local.zone1
+    availability_zone=locals.zone1
 
     tags = { 
         "Name" = "${locals.env}-private-${locals.zone1}"
@@ -14,7 +14,7 @@ resource "aws_subnet" "private_zone1" {
 resource "aws_subnet" "private_zone2" {
     vpc_id=aws_vpc.main.id
     cidr_block="10.0.32.0/19"
-    availability_zone=local.zone2
+    availability_zone=locals.zone2
 
     tags = { 
         "Name" = "${locals.env}-private-${locals.zone2}"
@@ -27,7 +27,7 @@ resource "aws_subnet" "private_zone2" {
 resource "aws_subnet" "public_zone1" {
     vpc_id=aws_vpc.main.id
     cidr_block="10.0.64.0/19"
-    availability_zone=local.zone1
+    availability_zone=locals.zone1
     map_public_ip_on_launch =true
 
     tags = { 
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_zone1" {
 resource "aws_subnet" "public_zone2" {
     vpc_id=aws_vpc.main.id
     cidr_block="10.0.96.0/19"
-    availability_zone=local.zone2
+    availability_zone=locals.zone2
     map_public_ip_on_launch =true
 
     tags = { 
